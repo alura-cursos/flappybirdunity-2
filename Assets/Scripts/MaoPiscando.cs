7b2377fbb46d92e5dcee0b454e1d4de8f4b99bfe
Ricardo <dones.bugan@gmail.com>
2018-03-22 17:21:42 -0300

    aula1

---
+++ Assets/Scripts/MaoPiscando.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaoPiscando : MonoBehaviour {
+    private SpriteRenderer imagem;
+	
+	private void Awake () {
+        this.imagem = this.GetComponent<SpriteRenderer>();
+	}
+	
+	private void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            this.Desaparecer();
+        }
+	}
+
+    private void Desaparecer()
+    {
+        this.imagem.enabled = false;
+    }
+}
